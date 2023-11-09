fa653f3a4bfc5371e1aa36323e8cd864a4f98d05
sashank <sashankrm11@gmail.com>
2023-11-09 14:52:46 -0500

    fixes

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.9-slim
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
+
+EXPOSE 8080
+
+ENV NAME World
+
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -123,6 +123,5 @@ if __name__ == "__main__":
 	env = os.environ.get('FLASK_ENV', 'development')
 	port = int(os.environ.get('PORT', 5000))
 	debug = False if env == 'production' else True
-	app.run(debug=True)
-	app.run(port=port, debug=debug)
-	# Careful with the debug mode..
+
+	app.run(host='0.0.0.0', debug=debug)
